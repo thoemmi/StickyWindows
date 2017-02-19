c95d99cab8b82a9b4fa8e5cb3a3d6ea1d061ba87
Thomas Freudenberg <info@thomasfreudenberg.com>
2017-02-19 23:17:42 +0100

    added behavior

---
+++ src/StickyWindows.WPF/StickyWindowBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace StickyWindows.WPF {
+    public class StickyWindowBehavior : Behavior<Window> {
+        private StickyWindow _stickyWindow;
+
+        protected override void OnAttached() {
+            _stickyWindow?.ReleaseHandle();
+
+            base.OnAttached();
+
+            if (AssociatedObject.IsLoaded) {
+                _stickyWindow = AssociatedObject.CreateStickyWindow();
+            } else {
+                AssociatedObject.Loaded += OnLoaded;
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs args) {
+            AssociatedObject.Loaded -= OnLoaded;
+            _stickyWindow = AssociatedObject.CreateStickyWindow();
+        }
+
+        protected override void OnDetaching() {
+            AssociatedObject.Loaded -= OnLoaded;
+            base.OnDetaching();
+            _stickyWindow?.ReleaseHandle();
+        }
+    }
+}
